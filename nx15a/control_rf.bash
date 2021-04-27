725ba8f7a17539cf7c4b0fd8d7fe3b837c263553
Robo Takao <37008294+RoboTakao@users.noreply.github.com>
2021-04-27 21:36:11 +0900

    Delete control_rf.bash

--- nx15a/control_rf.bash
+++
@@ -1,1 +0,0 @@
-rostopic pub /leg_lf_controller/command trajectory_msgs/JointTrajectory '{joint_names: ["joint_RR_1", "joint_RR_2", "joint_RR_3"], points: [{positions: [0.0, 0.0, 0.0], time_from_start: [1.0, 0.0]}]}'
