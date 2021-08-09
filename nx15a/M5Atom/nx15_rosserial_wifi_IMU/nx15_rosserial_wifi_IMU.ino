b2b87dbb806bf63cb4d0ba1f199d7c0f183617c8
Robo Takao <37008294+RoboTakao@users.noreply.github.com>
2021-08-09 16:18:10 +0900

    Add files via upload

---
+++ nx15a/M5Atom/nx15_rosserial_wifi_IMU/nx15_rosserial_wifi_IMU.ino
@@ -0,0 +1,201 @@
+#include "M5Atom.h"
+#include <Wire.h>          // I2C setting
+#include <PCA9685.h>       //for PCA9685
+#include <ros.h>
+#include <std_msgs/Int32.h>
+#include <std_msgs/Int32MultiArray.h>
+#include <sensor_msgs/JointState.h>
+#include <WiFi.h>
+
+
+//PCA9685のアドレス指定
+PCA9685 pwm = PCA9685(0x40);
+
+#define SERVOMIN 104            //Min pulse width (12bit 500μs) 
+#define SERVOMAX 512            //Max pulse width (12bit 2500μs) 
+
+float cont_min = -1570;
+float cont_max = 1570;
+int SrvAng[12] = {4914, 4914, 4914, 4914, 4914, 4914, 4914, 4914, 4914, 4914, 4914, 4914}; //90deg
+float TARGET_JOINT_POSITIONS[12] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+float TRIM[12] = {0.03, -0.07, -0.16, 0.03, -0.05, -0.03, -0.03, 0.1, 0.0, 0.07, 0.12, -0.12};
+int target_angle[12];
+
+const char* ssid = "XXXXXXXXXXXXX";
+const char* password = "XXXXXXXXXXXXX";
+
+WiFiClient client;
+IPAddress server(192, 168, 10, 19); //ROS core IP adress
+
+//int flag;
+//int Joy1_X, Joy1_Y, Joy2_X, Joy2_Y;
+//int ShiftX, ShiftY;
+
+//IMU
+float roll, pitch, yaw;
+#define IMU_AFS M5.IMU.AFS_2G       // Ascale [g]      (±2,4,8,16)
+#define IMU_GFS M5.IMU.GFS_250DPS  // Gscale [deg/s]  (±250,500,1000,200)
+int roll_initial, pitch_initial, yaw_initial;
+int initial_count = 100;
+
+class WiFiHardware {
+  public:
+    WiFiHardware() {};
+    void init() {
+      client.connect(server, 11411);
+    }
+    int read() {
+      return client.read();
+    }
+    void write(uint8_t* data, int length) {
+      for (int i = 0; i < length; i++)
+        client.write(data[i]);
+    }
+    unsigned long time() {
+      return millis(); // easy; did this one for you
+    }
+};
+
+ros::NodeHandle_<WiFiHardware> nh;
+
+std_msgs::Int32MultiArray IMU_data;
+
+void servo_cb(const sensor_msgs::JointState& msg) {
+
+  TARGET_JOINT_POSITIONS[0] = msg.position[6]; //FrontRight
+  TARGET_JOINT_POSITIONS[1] = -msg.position[7]; //FrontRight
+  TARGET_JOINT_POSITIONS[2] = -msg.position[8]; //FrontRight
+  TARGET_JOINT_POSITIONS[3] = msg.position[0]; //FrontLeft
+  TARGET_JOINT_POSITIONS[4] = msg.position[1]; //FrontLeft
+  TARGET_JOINT_POSITIONS[5] = msg.position[2]; //FrontLeft
+  TARGET_JOINT_POSITIONS[6] = msg.position[9]; //RearRight
+  TARGET_JOINT_POSITIONS[7] = -msg.position[10]; //RearRight
+  TARGET_JOINT_POSITIONS[8] = -msg.position[11]; //RearRight
+  TARGET_JOINT_POSITIONS[9] = msg.position[3]; //RearLeft
+  TARGET_JOINT_POSITIONS[10] = msg.position[4]; //RearLeft
+  TARGET_JOINT_POSITIONS[11] = msg.position[5]; //RearLeft
+
+  servo_set();
+
+  //for (int i=0; i <= 10; i++){
+  //  Serial.print(target_angle[i]);
+  //  Serial.print(" ");
+  //}
+  //Serial.println(target_angle[11]);
+
+}
+
+ros::Subscriber<sensor_msgs::JointState> sub("joint_states", servo_cb);
+ros::Publisher IMU_chatter("IMU_data", &IMU_data);
+
+void setupWiFi() {
+  WiFi.begin(ssid, password);
+  Serial.print("\nConnecting to "); Serial.println(ssid);
+  uint8_t i = 0;
+  while (WiFi.status() != WL_CONNECTED && i++ < 20) delay(500);
+  if (i == 21) {
+    Serial.print("Could not connect to"); Serial.println(ssid);
+    while (1) delay(500);
+  }
+  Serial.print("Ready! Use ");
+  Serial.print(WiFi.localIP());
+  Serial.println(" to access client");
+
+  M5.dis.drawpix(0, 0x00ff00);  //red
+  M5.dis.drawpix(0, 0x00ff00);  //red
+}
+
+void servo_set()
+{
+  for (int i = 0; i <= 11; i++) {
+    target_angle[i] = 1000 * (TARGET_JOINT_POSITIONS[i] + TRIM[i]);
+    SrvAng[i] = map(target_angle[i], cont_min, cont_max, SERVOMIN, SERVOMAX); //angle（-1.57～1.57rad）-> pulse width（150～500）
+    pwm.setPWM(i, 0, SrvAng[i]);
+  }
+}
+
+void angle_init()
+{
+  //initail_count 分だけ捨てる
+  for (int j = 1; j < initial_count; j++) {
+    M5.IMU.getAhrsData(&pitch, &roll, &yaw);
+    delay(10);
+  }
+  
+  //initail_count 分だけ初期値として平均値を求める
+  for (int i = 1; i <= initial_count; i++) {
+    M5.IMU.getAhrsData(&pitch, &roll, &yaw);
+    pitch_initial += pitch;
+    roll_initial += roll;
+    yaw_initial += yaw;
+    delay(10);
+  }
+  pitch_initial /= initial_count;
+  roll_initial /= initial_count;
+  yaw_initial /= initial_count;
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  // void M5Atom::begin(bool SerialEnable , bool I2CEnable , bool DisplayEnable )
+  M5.begin(true, false, true);
+
+  setupWiFi();
+
+  Wire.begin(19, 22); //SDA-19, SCL-22
+
+  pwm.begin();                   //initial setting (for 0x40) (PCA9685)
+  pwm.setPWMFreq(50);            //PWM 50Hz (for 0x40) (PCA9685)
+
+  servo_set();
+
+  M5.dis.drawpix(0, 0x0000ff);  //blue
+  M5.dis.drawpix(0, 0x0000ff);  //blue
+
+  Serial.println("Waiting for connections...");
+
+  IMU_data.data_length = 3;
+  IMU_data.data = (int32_t *)malloc(sizeof(int32_t) * 3);
+
+  M5.IMU.Init();
+  M5.IMU.SetGyroFsr(IMU_GFS);
+  M5.IMU.SetAccelFsr(IMU_AFS);
+
+  nh.initNode();
+  nh.subscribe(sub);
+  nh.advertise(IMU_chatter);
+
+  M5.dis.drawpix(0, 0xff0000); //green
+  M5.dis.drawpix(0, 0xff0000); //green
+
+  angle_init();
+
+  for (uint32_t i = 0; i < 25; i++) {
+    M5.dis.drawpix(i, 0xff0000); //green
+  }
+
+  xTaskCreatePinnedToCore(IMU_get, "IMU_get", 4096, NULL, 5, NULL, 1);
+}
+
+void IMU_get(void *pvParameters)
+{
+  while(1){
+    M5.IMU.getAhrsData(&pitch, &roll, &yaw);
+    pitch = pitch - pitch_initial;
+    roll = roll - roll_initial;
+    yaw = yaw - yaw_initial;
+    Serial.printf("pitch:%.2f,roll:%.2f,yaw:%.2f\r\n", pitch, roll, yaw);
+    IMU_data.data[0] = int(pitch);
+    IMU_data.data[1] = int(roll);
+    IMU_data.data[2] = int(yaw);
+    IMU_chatter.publish( &IMU_data );
+    delay(100);
+  }
+}
+
+void loop()
+{
+  nh.spinOnce();
+  delay(1);
+}
---
+++ nx15a/M5Atom/nx15_rosserial_wifi_wii/nx15_rosserial_wifi_wii.ino
@@ -0,0 +1,195 @@
+#include "M5Atom.h"
+#include <Wire.h>
+#include <ros.h>
+#include <std_msgs/Int32MultiArray.h>
+#include <WiFi.h>
+
+int loop_cnt=0;
+
+//byte accx,accy,accz,joyx,joyy,zbut,cbut;
+
+static uint8_t nunchuck_buf[6];   // array to store nunchuck data,
+
+WiFiClient client;
+IPAddress server(192, 168, 10, 19); //ROS core IP adress
+
+const char* ssid = "XXXXXXXXXXXXX";
+const char* password = "XXXXXXXXXXXXX";
+
+// initialize the I2C system, join the I2C bus,and tell the nunchuck we're talking to it
+static void nunchuck_init()
+{ 
+    Wire.begin(19, 22);                // join i2c bus as master; for M5Atom SDA-19, SCL-22
+    Wire.beginTransmission(0x52);// transmit to device 0x52
+    Wire.write((uint8_t)0x40);// sends memory address
+    Wire.write((uint8_t)0x00);// sends sent a zero.  
+    Wire.endTransmission();// stop transmitting
+}
+
+// Send a request for data to the nunchuck was "send_zero()"
+static void nunchuck_send_request()
+{
+    Wire.beginTransmission(0x52);// transmit to device 0x52
+
+    Wire.write((uint8_t)0x00);// sends one byte
+
+    Wire.endTransmission();// stop transmitting
+}
+
+// Encode data to format that most wiimote drivers except only needed if you use one of the regular wiimote drivers
+static char nunchuk_decode_byte (char x)
+{
+    x = (x ^ 0x17) + 0x17;
+    return x;
+}
+
+// Receive data back from the nunchuck, returns 1 on successful read. returns 0 on failure
+static int nunchuck_get_data()
+{
+    int cnt=0;
+    Wire.requestFrom (0x52, 6);// request data from nunchuck
+    while (Wire.available ()) {
+        // receive byte as an integer
+        nunchuck_buf[cnt] = nunchuk_decode_byte( Wire.read() );
+        cnt++;
+    }
+    nunchuck_send_request();  // send request for next data payload
+    // If we recieved the 6 bytes, then go print them
+    if (cnt >= 5) {
+        return 1;   // success
+    }
+    return 0; //failure
+}
+
+// returns zbutton state: 1=pressed, 0=notpressed
+static int nunchuck_zbutton()
+{
+    return ((nunchuck_buf[5] >> 0) & 1) ? 0 : 1;  // voodoo
+}
+
+// returns zbutton state: 1=pressed, 0=notpressed
+static int nunchuck_cbutton()
+{
+    return ((nunchuck_buf[5] >> 1) & 1) ? 0 : 1;  // voodoo
+}
+
+// returns value of x-axis joystick
+static int nunchuck_joyx()
+{
+    return nunchuck_buf[0]; 
+}
+
+// returns value of y-axis joystick
+static int nunchuck_joyy()
+{
+    return nunchuck_buf[1];
+}
+
+// returns value of x-axis accelerometer
+static int nunchuck_accelx()
+{
+    return nunchuck_buf[2];   // FIXME: this leaves out 2-bits of the data
+}
+
+// returns value of y-axis accelerometer
+static int nunchuck_accely()
+{
+    return nunchuck_buf[3];   // FIXME: this leaves out 2-bits of the data
+}
+
+// returns value of z-axis accelerometer
+static int nunchuck_accelz()
+{
+    return nunchuck_buf[4];   // FIXME: this leaves out 2-bits of the data
+}
+
+class WiFiHardware {
+  public:
+    WiFiHardware() {};
+    void init() {
+      client.connect(server, 11412);   
+    }
+    int read() {
+      return client.read();      
+    }
+    void write(uint8_t* data, int length) {
+      for (int i = 0; i < length; i++)
+        client.write(data[i]);
+    }
+    unsigned long time() {
+      return millis(); // easy; did this one for you
+    }
+};
+
+ros::NodeHandle_<WiFiHardware> nh;
+
+std_msgs::Int32MultiArray Wii_joy;
+
+ros::Publisher chatter("Wii_joystick", &Wii_joy);
+
+void setupWiFi() {
+  WiFi.begin(ssid, password);
+  Serial.print("\nConnecting to "); Serial.println(ssid);
+  uint8_t i = 0;
+  while (WiFi.status() != WL_CONNECTED && i++ < 20) delay(500);
+  if (i == 21) {
+    Serial.print("Could not connect to"); Serial.println(ssid);
+    while (1) delay(500);
+  }
+  Serial.print("Ready! Use ");
+  Serial.print(WiFi.localIP());
+  Serial.println(" to access client");
+
+  M5.dis.drawpix(0, 0x00ff00);  //red
+  M5.dis.drawpix(0, 0x00ff00);  //red
+}
+
+void setup()
+{
+    Serial.begin(19200);
+    // void M5Atom::begin(bool SerialEnable , bool I2CEnable , bool DisplayEnable )
+    M5.begin(true, false, true);
+    
+    //nunchuck_setpowerpins();
+    nunchuck_init(); // send the initilization handshake
+    
+    setupWiFi();
+
+    Serial.println("Waiting for connections...");
+
+    Wii_joy.data_length = 4;
+    Wii_joy.data = (int32_t *)malloc(sizeof(int32_t)*3);
+  
+    nh.initNode();
+    nh.advertise(chatter);
+
+    M5.dis.drawpix(0, 0xff0000); //green
+}
+
+void loop()
+{
+    nunchuck_get_data();
+
+    int zbut = nunchuck_zbutton();
+    int cbut = nunchuck_cbutton(); 
+    int joyx = map(nunchuck_joyx(),23,225,-15,15);
+    if(abs(joyx)<=1) joyx=0;
+    int joyy = map(nunchuck_joyy(),20,221,-15,15);
+    if(abs(joyy)<=1) joyy=0;
+
+    Serial.print(" zbut: "); Serial.print(zbut);
+    Serial.print(" cbut: "); Serial.print(cbut);
+    Serial.print(" joyx: "); Serial.print(joyx);
+    Serial.print(" joyy: "); Serial.println(joyy);
+
+    Wii_joy.data[0] = zbut;
+    Wii_joy.data[1] = cbut;
+    Wii_joy.data[2] = joyx;
+    Wii_joy.data[3] = joyy;
+
+    chatter.publish( &Wii_joy );
+  
+    nh.spinOnce();
+
+    delay(50);
+}
